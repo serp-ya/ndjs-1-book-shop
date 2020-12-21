02d79f13ea5abf99d44f5fb3cf98c96adfaf56bb
Sergei Kundryukov <sergei.kundryukov1989@gmail.com>
2020-12-21 22:38:33 +0300

    Change app port in env file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { server } from './server';
 import { APP_DEFAULT_PORT } from './constants';
 
-const appPort = process.env.APP_PORT || APP_DEFAULT_PORT;
+const appPort = process.env.PORT || APP_DEFAULT_PORT;
 
 server.listen(appPort, () => {
     process.stdout.write(`Server started on ${appPort} port!\n`);
