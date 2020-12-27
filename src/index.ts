1a30751670c68986fc0ccfcc590656ce62c9d65a
Sergei Kundryukov <sergei.kundryukov1989@gmail.com>
2020-12-27 17:45:20 +0300

    Change evn vars handling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { server } from './server';
 import { APP_DEFAULT_PORT } from './constants';
+import { PORT } from './env';
 
-const appPort = process.env.PORT || APP_DEFAULT_PORT;
+const appPort = PORT || APP_DEFAULT_PORT;
 
 server.listen(appPort, () => {
     process.stdout.write(`Server started on ${appPort} port!\n`);
--- src/routes/utils/book-views-counter/book-views-counter-constants.ts
+++ src/routes/utils/book-views-counter/book-views-counter-constants.ts
@@ -1,3 +1,5 @@
-export const UPDATE_VIEW_COUNTER_URL = process.env.VIEW_COUNTER_URL
-    ? `${process.env.VIEW_COUNTER_URL}/counter`
+import { VIEW_COUNTER_URL } from '@/env';
+
+export const UPDATE_VIEW_COUNTER_URL = VIEW_COUNTER_URL
+    ? `${VIEW_COUNTER_URL}/counter`
     : undefined;
